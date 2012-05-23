4cbf585ccb410e70402257d8bcc475376da2cac3
Uģis Ozols <ugis.ozolss@gmail.com>
2012-05-23 16:15:08 +0300

    Make our overridden devise routes to be matched first.

--- authentication/config/routes.rb
+++ authentication/config/routes.rb
@@ -1,6 +1,15 @@
 Refinery::Core::Engine.routes.draw do
   begin
     require 'devise'
+    
+    # Override Devise's other routes for convenience methods.
+    devise_scope :refinery_user do
+      get '/refinery/login', :to => "sessions#new", :as => :new_refinery_user_session
+      get '/refinery/logout', :to => "sessions#destroy", :as => :destroy_refinery_user_session
+      get '/refinery/users/register' => 'users#new', :as => :new_refinery_user_registration
+      post '/refinery/users/register' => 'users#create', :as => :refinery_user_registration
+    end
+
     devise_for :refinery_user,
                :class_name => 'Refinery::User',
                :path => 'refinery/users',
@@ -10,13 +19,6 @@ Refinery::Core::Engine.routes.draw do
                                 :sign_in => 'login',
                                 :sign_up => 'register' }
 
-    # Override Devise's other routes for convenience methods.
-    devise_scope :refinery_user do
-      get '/refinery/login', :to => "sessions#new", :as => :new_refinery_user_session
-      get '/refinery/logout', :to => "sessions#destroy", :as => :destroy_refinery_user_session
-      get '/refinery/users/register' => 'users#new', :as => :new_refinery_user_registration
-      post '/refinery/users/register' => 'users#create', :as => :refinery_user_registration
-    end
   rescue RuntimeError => exc
     if exc.message =~ /ORM/
       # We don't want to complain on a fresh installation.
