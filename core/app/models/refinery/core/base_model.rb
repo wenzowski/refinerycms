c4a75ccdfb8c5fb79727448e7b621d25d0c7ccda
Philip Arndt <parndt@gmail.com>
2012-05-23 21:30:23 +1200

    Use github source for orm_adapter and manually extend it because it was not doing it automatically.

--- core/app/models/refinery/core/base_model.rb
+++ core/app/models/refinery/core/base_model.rb
@@ -1,6 +1,12 @@
+require 'orm_adapter'
+
 module Refinery
   module Core
     class BaseModel < ActiveRecord::Base
+
+      # TODO
+      extend ::OrmAdapter::ToAdapter
+      self::OrmAdapter = ::OrmAdapter::ActiveRecord
 
       self.abstract_class = true
 
